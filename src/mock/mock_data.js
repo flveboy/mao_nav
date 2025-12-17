0b231695606e3cf767253c1e9b14efaeb598b572
龙龙啊 <flveboy@163.com>
2025-12-17 13:12:35 +0800

    chore: auto-sync bookmarks from Gitee (2025-12-17T05:12:35.544Z)

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -991,6 +991,14 @@ export const mockData = {
           "_source": "gitee_bookmark"
         },
         {
+          "id": "site-tda1oytr-1765948355308",
+          "name": "MoonTV",
+          "url": "https://tv.crabpt.vip/",
+          "description": "MoonTV",
+          "icon": "/sitelogo/tv.crabpt.vip.ico",
+          "_source": "gitee_bookmark"
+        },
+        {
           "id": "site-n1on4mub-1765788994491",
           "name": "夸克网盘音乐资源_无损音乐/热门歌曲下载 | 华语/欧美/日韩专辑合集–夸克网盘社",
           "url": "https://kuakes.com/music",
