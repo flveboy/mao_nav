dc1f4cc9f16432caf929bf8d3c9e1bdc39066c4b
龙龙啊 <flveboy@163.com>
2025-12-17 13:13:21 +0800

    chore: auto-sync bookmarks from Gitee (2025-12-17T05:13:21.247Z)

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -1015,6 +1015,14 @@ export const mockData = {
           "_source": "gitee_bookmark"
         },
         {
+          "id": "site-vt6eu9b1-1765948400179",
+          "name": "MoviePilot",
+          "url": "https://mp.crabpt.vip/#/subscribe/movie",
+          "description": "MoviePilot",
+          "icon": "/sitelogo/mp.crabpt.vip.ico",
+          "_source": "gitee_bookmark"
+        },
+        {
           "id": "site-iq4ttm96-1765933937918",
           "name": "云盘分享网 www.ypfxw.com，网盘资源分享网站",
           "url": "http://www.alypw.cn/",
